3e3eb09ed51c06afa1a270d040e6ff73ebcca9b3
Ilya <boss.malinovskiy@gmail.com>
2018-06-10 21:35:22 +0500

    Steam+OpenID

---
+++ WebApplication2/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication2.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="AuthorizeButton" runat="server" Text="Авторизация через Steam" OnClick="AuthorizeButton_Click" Width="192px" />
+        </div>
+        <p>
+            <asp:Label ID="StatusLabel" runat="server" Text=""></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebApplication2/Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using DotNetOpenAuth.OpenId.RelyingParty;
+
+namespace WebApplication2
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var openId = new OpenIdRelyingParty();
+            var response = openId.GetResponse();
+
+            if (response != null)
+            {
+                switch (response.Status)
+                {
+                    case AuthenticationStatus.Authenticated:
+                        StatusLabel.Text = $"Вы авторизованы под пользователем c ID {response.ClaimedIdentifier.ToString()}";
+                        break;
+                    case AuthenticationStatus.Canceled:
+                        StatusLabel.Text = $"Авторизация не удалась";
+                        break;
+                    case AuthenticationStatus.Failed:
+                        StatusLabel.Text = $"Авторизация не удалась";
+                        break;
+                }
+            }
+            else
+            {
+                StatusLabel.Text = $"Вы не авторизованы";
+            }
+        }
+
+        protected void AuthorizeButton_Click(object sender, EventArgs e)
+        {
+            Authorize();
+        }
+        protected void Authorize()
+        {          
+            using (OpenIdRelyingParty openId = new OpenIdRelyingParty())
+            {
+                IAuthenticationRequest request = openId.CreateRequest("http://steamcommunity.com/openid");
+                request.RedirectToProvider();
+            }   
+        }
+
+        
+    }
+}
